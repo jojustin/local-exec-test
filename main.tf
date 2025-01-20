2ec98998f07db0b69e37bb34c97978528f6bbacf
AnilCM <56347169+Anil-CM@users.noreply.github.com>
2025-01-20 19:56:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,2 @@
-resource "null_resource" "test" {
-    connection {
-    type     = "ssh"
-    user     = "root"
-    private_key = var.ssh_private_key
-    host     = var.host_ip
-  }
-    provisioner "remote-exec" {
-    inline = [
-     "echo 'hello'"
-    ]
-    timeout = 100
-  }
-}
+data "external" "example" {
+  program = ["sh", "cat" "/etc/passwd"] }
